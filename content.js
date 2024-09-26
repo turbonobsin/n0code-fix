04a072c1770f50356a569dd7b82a5b0130a098c5
Claeb <87375599+turbonobsin@users.noreply.github.com>
2024-09-26 16:39:50 -0400

    custom folders with image support

--- content.js
+++ content.js
@@ -1,6 +1,119 @@
 console.log("n0code tweaks loaded");
 document.body.style.overflow = "hidden";
 
+class RegImg{
+    static async create(/**@type {File}*/f){
+        let t = new RegImg();
+        t.name = f.name;
+        t.f = f;
+
+        let url = URL.createObjectURL(t.f);
+        t.url = url;
+
+        t.buf = Array.from(new Uint8Array(await f.arrayBuffer()));
+
+        return t;
+    }
+    /**@type {File} */
+    f;
+    name = "";
+    url;
+    /**@type {Uint8Array} */
+    buf;
+    
+    getID(){
+        let name = this.name.replace(/\./g,"_");
+        return "--img-"+name;
+    }
+    register(){
+        let id = this.getID();
+        console.log("> registered img with id: ",id);
+        document.body.parentElement.style.setProperty(id,`url(${this.url})`);
+    }
+
+    serialize(){
+        return {
+            name:this.name,
+            buf:this.buf
+        };
+    }
+    static deserialize(data){
+        let t = new RegImg();
+        t.buf = new Uint8Array(data.buf);
+        t.name = data.name;
+        t.f = new File([t.buf],t.name);
+        t.url = URL.createObjectURL(t.f);
+        return t;
+    }
+}
+class RegCSS{
+    static async create(/**@type {File}*/f){
+        let t = new RegCSS();
+
+        t.name = f.name;
+        t.content = await f.text();
+        
+        return t;
+    }
+    name = "";
+    content = "";
+    
+    serialize(){
+        return {
+            name:this.name,
+            content:this.content
+        };
+    }
+    static deserialize(data){
+        let t = new RegCSS();
+        t.name = data.name;
+        t.content = data.content;
+        return t;
+    }
+
+    register(){
+        addCustomCSS(this.content);
+    }
+}
+
+class CustomPack{
+    /**@type {RegImg[]} */
+    images = [];
+
+    /**@type {RegCSS[]} */
+    css = [];
+
+    save(){
+        let o = {
+            images:this.images.map(v=>v.serialize()),
+            css:this.css.map(v=>v.serialize())
+        };
+        let s = JSON.stringify(o);
+        localStorage.setItem("__customPack",s);
+    }
+    static load(pack){
+        let p = new CustomPack();
+        for(const a of pack.images){
+            p.images.push(RegImg.deserialize(a));
+        }
+        for(const a of pack.css){
+            p.css.push(RegCSS.deserialize(a));
+        }
+
+        p.registerAll();
+        return p;
+    }
+
+    registerAll(){
+        for(const a of this.css){
+            a.register();
+        }
+        for(const img of this.images){
+            img.register();
+        }
+    }
+}
+
 document.addEventListener("keydown",async e=>{
     if(e.altKey){
         if(e.key == "c"){ // apply custom CSS
@@ -20,8 +133,56 @@ document.addEventListener("keydown",async e=>{
             localStorage.clear();
             clearCSS();
         }
+        else if(e.key == "f"){
+            let handle = await showDirectoryPicker({
+                id:"custom_folder",
+                mode:"read"
+            });
+            if(!handle) return;
+
+            let res = await handle.requestPermission({mode:"read"});
+            if(res == "denied") return;
+
+            let reg = new CustomPack();
+            async function search(/**@type {FileSystemDirectoryHandle}*/handle){
+                for await(const [name,h] of handle.entries()){
+                    console.log("n: ",name,h.kind);
+                    let ext = name.split(".").pop();
+                    if(h.kind == "file"){
+                        let f = await h.getFile();
+                        if(f.type.startsWith("image/")){
+                            reg.images.push(await RegImg.create(f));
+                        }
+                        else if(ext == "css"){
+                            let r = await RegCSS.create(f);
+                            reg.css.push(r);
+                        }
+                    }
+                    else await search(h);
+                }
+            }
+            await search(handle);
+
+            console.log("REG:",reg);
+
+            reg.registerAll();
+            reg.save();
+        }
     }
 });
+
+function loadPack(){
+    let pack = localStorage.getItem("__customPack");
+    if(!pack){
+        console.log("Failed to load custom pack, no pack was found.");
+        return;
+    }
+    let o = JSON.parse(pack);
+
+    let p = CustomPack.load(o);
+    console.log("loaded: ",p);
+}
+loadPack();
 
 function clearCSS(){
     let existing = document.querySelector(".custom-style");
@@ -29,7 +190,9 @@ function clearCSS(){
 }
 function loadCustomCSS(s=""){
     clearCSS();
-    
+    addCustomCSS(s);
+}
+function addCustomCSS(s=""){
     let customStyle = document.createElement("style");
     customStyle.rel = "stylesheet";
     customStyle.innerHTML = s;
