552e99904ad71a230e4503592bc685bc44402717
Claeb <87375599+turbonobsin@users.noreply.github.com>
2024-09-25 21:10:01 -0400

    update 2

--- background.js
+++ background.js
@@ -1,18 +1,3 @@
 chrome.runtime.onInstalled.addListener(() => {
     console.log('Extension installed');
 });
-
-// chrome.scripting.registerContentScripts([
-//     {
-//         // js: [{ file: "/content2.js" }],
-//         // matches: ["<all_urls>"],
-//         // allFrames: true,
-//         // runAt:"document_start",
-
-//         id:"n0code-tweaks",
-//         runAt:"document_start",
-//         // matches:["https://n0code.net/work/teaching/courses/index.html"],
-//         matches: ["<all_urls>"],
-//         js:["/content2.js"]
-//     }
-// ]);
--- content.js
+++ content.js
@@ -1,18 +1,25 @@
 console.log("n0code tweaks loaded");
+document.body.style.overflow = "hidden";
 
 class Obj{
     constructor(/**@type {HTMLElement}*/e){
         this.ref = e.cloneNode(true);
         let a = this.ref.querySelector("a");
-
-        // this.ref.style.transform = "translate(-50%,-50%)";
         
         this.link = a.href;
         this.text = a.textContent;
-        this.w = this.ref.getBoundingClientRect().width/2;
+        this.w = e.getBoundingClientRect().width/2;
 
         this.x = Math.random()*(innerWidth-150);
         this.y = Math.random()*(innerHeight-150);
+
+        this.ref.addEventListener("mouseenter",e=>{
+            this.vx = 0;
+            this.vy = 0;
+            this.ax = 0;
+            this.ay = 0;
+            this.update();
+        });
     }
     link = "";
     text = "";
@@ -41,8 +48,12 @@ class Obj{
 /**@type {Obj[]} */
 let objs = [];
 
-let aDrag = 0.75;
-let vDrag = 0.75;
+let aDrag = 0.9995;
+let vDrag = 0.9995;
+// let aDrag = 0.98;
+// let vDrag = 0.98;
+let colDrag = 0.8;
+let maxVel = 1.5;
 
 let all = document.querySelectorAll(".roundbutton");
 for(const a of all){
@@ -58,11 +69,9 @@ function run(){
     for(const o of objs){
         let lx = o.x;
         let ly = o.y;
-        // o.x = Math.random()*(innerWidth-150);
-        // o.y = Math.random()*(innerHeight-150);
 
         let ang = Math.random()*6.28;
-        let amt = 20;
+        let amt = 0.05;
         o.ax = Math.cos(ang)*amt;
         o.ay = Math.sin(ang)*amt;
 
@@ -85,8 +94,7 @@ function update(){
 
     for(const a of objs){
         a.ref.style.backgroundColor = null;
-        // a.ref.style.transition = `top 5s ease-in-out, left 5s ease-in-out, background-color 1s`;
-        a.ref.style.transition = `top 5s ease-in-out, left 5s ease-in-out`;
+        a.ref.style.transition = "none";
 
         a.ax *= aDrag;
         a.ay *= aDrag;
@@ -95,27 +103,34 @@ function update(){
 
         a.vx += a.ax;
         a.vy += a.ay;
-        
-        a.x += a.vx;
-        a.y += a.vy;
 
-        let margin = a.w;
-        if(a.x < margin){
-            a.x = margin;
-            a.vx = Math.abs(a.ax);
+        let margin = a.w*2;
+        if(a.x < 0){
+            a.x = 0;
+            a.vx = Math.abs(a.vx);
         }
         else if(a.x >= innerWidth-margin){
             a.x = innerWidth-margin-1;
             a.vx = -Math.abs(a.vx);
         }
-        if(a.y < margin){
-            a.y = margin;
-            a.vy = Math.abs(a.ay);
+        if(a.y < 0){
+            a.y = 0;
+            a.vy = Math.abs(a.vy);
         }
         else if(a.y >= innerHeight-margin){
             a.y = innerHeight-margin-1;
             a.vy = -Math.abs(a.vy);
         }
+
+        if(a.vx > maxVel) a.vx = maxVel;
+        else if(a.vx < -maxVel) a.vx = -maxVel;
+        if(a.vy > maxVel) a.vy = maxVel;
+        else if(a.vy < -maxVel) a.vy = -maxVel;
+        
+        a.x += a.vx;
+        a.y += a.vy;
+
+        a.update();
     }
 
     if(true) for(let i = 0; i < objs.length; i++){
@@ -123,67 +138,41 @@ function update(){
         for(let j = i+1; j < objs.length; j++){
             let o2 = objs[j];
 
-            let dx = o2.x-o1.x;
-            let dy = o2.y-o1.y;
+            let dx = (o2.x+o2.w)-(o1.x+o1.w);
+            let dy = (o2.y+o2.w)-(o1.y+o1.w);
             let dist = Math.sqrt(dx**2+dy**2);
             let ang = Math.atan2(dy,dx);
 
             if(dist < o1.w+o2.w){
-                // o1.vx -= dx/2;
-                // o1.vy -= dy/2;
-                // o2.vx += dx/2;
-                // o2.vy += dy/2;
-                o1.x -= dx/2;
-                o1.y -= dy/2;
-                o2.x += dx/2;
-                o2.y += dy/2;
-            }
-        }
-    }
-    
-    if(false) for(let i = 0; i < objs.length; i++){
-        let o1 = objs[i];
-        let r1 = o1.ref.getBoundingClientRect();
-        // if(t-o1.lastCol < 500) continue;
-        for(let j = i+1; j < objs.length; j++){
-            let o2 = objs[j];
-            // if(t-o2.lastCol < 500) continue;
-            let r2 = o2.ref.getBoundingClientRect();
+                let dif = (o2.w+o1.w) - dist;
+                o1.x -= Math.cos(ang)*dif;
+                o1.y -= Math.sin(ang)*dif;
+                o2.x += Math.cos(ang)*dif;
+                o2.y += Math.sin(ang)*dif;
 
-            let dx = r2.x-r1.x;
-            let dy = r2.y-r1.y;
-            let dist = Math.sqrt(dx**2+dy**2);
-            let ang = Math.atan2(dy,dx);
+                let bounceAmt1 = Math.sqrt(o1.vx**2+o1.vy**2) * colDrag;
+                let bounceAmt2 = Math.sqrt(o2.vx**2+o2.vy**2) * colDrag;
 
-            if(dist < o1.w+o2.w){
-                // collision
+                o1.vx = Math.cos(ang+Math.PI)*bounceAmt1;
+                o1.vy = Math.sin(ang+Math.PI)*bounceAmt1;
+                o2.vx = Math.cos(ang)*bounceAmt2;
+                o2.vy = Math.sin(ang)*bounceAmt2;
 
-                // o1.lastCol = performance.now();
-                // o2.lastCol = performance.now();
-
-                o1.ref.style.backgroundColor = "black";
-                o2.ref.style.backgroundColor = "black";
-
-                let prog = (t-timeStart)/8000 / 4;
-                o1.x -= Math.cos(ang)*o1.ldist*prog;
-                o1.y -= Math.sin(ang)*o1.ldist*prog;
-                o2.x += Math.cos(ang)*o2.ldist*prog;
-                o2.y += Math.sin(ang)*o2.ldist*prog;
+                // probably should optimize these all with proportions to replace all the trig functions at some point
+                
                 o1.update();
                 o2.update();
             }
         }
     }
 }
-update();
 
 setInterval(()=>{
     run();
 },8000);
-run();
 setTimeout(()=>{
     run();
 },500);
-// setTimeout(()=>{
-    // update();
-// },2500);
+
+update();
+run();
